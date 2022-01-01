a04e20c6d405818766985284b28d9c1e852cc0f4
buingoclam147 <buingoclam00@gmail.com>
2022-01-01 14:10:53 +0700

    update route footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTER_CONST } from '../const/router.const';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  onRouter(i): void {
+    if (i === 'Trang chủ') {
+      this.router.navigate([ROUTER_CONST['Trang chủ']]);
+    }
+    else {
+      this.router.navigate([ROUTER_CONST[i.target.outerText]]);
+    }
+  }
 }
